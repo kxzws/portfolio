51d2e690851cb12ed4463f1cd9f8eebfeadbdf19
Alena Bulanova <kxzws54@gmail.com>
2021-09-27 20:12:56 +0300

    fix: repair volume-progress bar

--- museum/js/script.js
+++ museum/js/script.js
@@ -1,6 +1,13 @@
-const progress = document.querySelector('.video__progress');
+const playProgress = document.querySelector('.play-progress');
   
-progress.addEventListener('input', function() {
+playProgress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
+})
+
+const volumeProgress = document.querySelector('.volume-progress');
+  
+volumeProgress.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
 })

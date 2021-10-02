29a3151907c205168552a18f8c2ecf8fe4bad10f
Alena Bulanova <kxzws54@gmail.com>
2021-10-02 22:51:48 +0300

    fix: add booking form cards input, change font vars

--- museum/js/script.js
+++ museum/js/script.js
@@ -1,13 +1,39 @@
-const playProgress = document.querySelector('.play-progress');
-  
-playProgress.addEventListener('input', function() {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
-})
+const playProgress = document.querySelector(".play-progress");
 
-const volumeProgress = document.querySelector('.volume-progress');
-  
-volumeProgress.addEventListener('input', function() {
+playProgress.addEventListener("input", function () {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
-})
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+});
+
+const volumeProgress = document.querySelector(".volume-progress");
+
+volumeProgress.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+});
+
+function createRipple(event) {
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+const buttons = document.getElementsByClassName("modal-btn");
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}

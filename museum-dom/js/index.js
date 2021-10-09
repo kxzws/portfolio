0364ba85b77807c98bb8b1f49f07cbc24392014b
Alena Bulanova <kxzws54@gmail.com>
2021-10-09 23:03:53 +0300

    refac: add js modules

---
+++ museum-dom/js/index.js
@@ -0,0 +1,20 @@
+import startSlider from "./welcome.js";
+startSlider();
+
+import startMenu from "./header.js";
+document.addEventListener("click", startMenu);
+
+import { progressAnimation } from "./video.js";
+const playProgress = document.querySelector(".play-progress"),
+  volumeProgress = document.querySelector(".volume-progress");
+playProgress.addEventListener("input", progressAnimation);
+volumeProgress.addEventListener("input", progressAnimation);
+
+import { createRipple } from "./tickets.js";
+const buttons = document.getElementsByClassName("modal-btn");
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
+
+import initComparisons from "./explore.js";
+initComparisons();
---
+++ museum-dom/js/video.js
@@ -0,0 +1,7 @@
+//progress-bar in video
+function progressAnimation() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+}
+
+export { progressAnimation };

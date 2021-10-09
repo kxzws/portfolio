3e906bd893db792993cf93ee8c242f45afbcbec3
Alena Bulanova <kxzws54@gmail.com>
2021-10-09 21:52:56 +0300

    feat: add welcome slider

--- museum-adaptive/js/script.js
+++ museum-adaptive/js/script.js
@@ -1,13 +1,3 @@
-console.log("Score: 150/150");
-console.log("Не выполненные/не засчитанные пункты:");
-console.log("1) Результат проверки скорости сайта для мобильных устройств");
-console.log("Частично выполненные пункты:");
-console.log("1) слайдера сравнения изображений в секции `Explore`");
-console.log("feedback: размеры блока изменяются плавно, но сами картинки плывут между брейкпоинтами");
-console.log("2) при клике по ссылке в адаптивном меню, или при клике по любому месту сайта,");
-console.log("кроме самого адаптивного меню, меню закрывается");
-console.log("feedback: меню закрывается при клике по любому месту сайта, но не при кликах по ссылкам");
-
 //responsive menu in header-nav
 const burgerMenu = document.querySelector(".header__nav");
 const closeMenu = document.querySelector(".header__close");
@@ -20,29 +10,86 @@ const secondLine = document.querySelector(".line-second");
 const thirdLine = document.querySelector(".line-third");
 
 function toggleMenu() {
-  burgerMenu.classList.toggle('open');
-  welcomeSideBar.classList.toggle('side-bar-unvis');
-  welcomeCarousel.classList.toggle('carousel-unvis');
-  welcomeCarouselPanel.classList.toggle('carousel-unvis');
-  firstLine.classList.toggle('line-first-close');
-  secondLine.classList.toggle('line-second-close');
-  thirdLine.classList.toggle('line-third-close');
+  burgerMenu.classList.toggle("open");
+  welcomeSideBar.classList.toggle("side-bar-unvis");
+  welcomeCarousel.classList.toggle("carousel-unvis");
+  welcomeCarouselPanel.classList.toggle("carousel-unvis");
+  firstLine.classList.toggle("line-first-close");
+  secondLine.classList.toggle("line-second-close");
+  thirdLine.classList.toggle("line-third-close");
 }
-closeMenu.addEventListener('click', e => {
+closeMenu.addEventListener("click", (e) => {
   e.stopPropagation();
   toggleMenu();
 });
 
-document.addEventListener('click', e => {
+document.addEventListener("click", (e) => {
   let target = e.target;
   let its_menu = target == burgerMenu || burgerMenu.contains(target);
   let its_hamburger = target == closeMenu;
-  let menu_is_active = burgerMenu.classList.contains('open');
-  
+  let menu_is_active = burgerMenu.classList.contains("open");
+
   if (!its_menu && !its_hamburger && menu_is_active) {
     toggleMenu();
   }
-})
+});
+
+//slider in welcome
+const prev = document.getElementById("btn-prev"),
+  next = document.getElementById("btn-next"),
+  slides = document.querySelectorAll(".carousel__item"),
+  dots = document.querySelectorAll(".carousel__check"),
+  order = document.querySelector(".carousel__order-digit");
+
+let index = 0; // текущий активный слайд
+
+const activeSlide = (n) => {
+  for (let slide of slides) {
+    slide.classList.remove("item-active");
+  }
+  slides[n].classList.add("item-active");
+  order.textContent = `0${n + 1}`;
+};
+const activeDot = (n) => {
+  for (let dot of dots) {
+    dot.classList.remove("check-active");
+  }
+  dots[n].classList.add("check-active");
+};
+const prepareCurrentSlide = (ind) => {
+  activeSlide(ind);
+  activeDot(ind);
+};
+const nextSlide = () => {
+  if (index === slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+};
+const prevSlide = () => {
+  if (index === 0) {
+    index = slides.length - 1;
+    prepareCurrentSlide(index);
+  } else {
+    index--;
+    prepareCurrentSlide(index);
+  }
+};
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener("click", () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  });
+});
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+
+setInterval(nextSlide, 10000);
 
 //progress-bar in video
 const playProgress = document.querySelector(".play-progress");

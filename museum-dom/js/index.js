85a91bf08b141a7a9ab14011b820424caba59c7a
Alena Bulanova <kxzws54@gmail.com>
2021-10-11 19:40:20 +0300

    feat: add mapbox in contacts section

--- museum-dom/js/index.js
+++ museum-dom/js/index.js
@@ -4,18 +4,47 @@ startSlider();
 import startMenu from "./header.js";
 document.addEventListener("click", startMenu);
 
+import initComparisons from "./explore.js";
+initComparisons();
+
 import { progressAnimation } from "./video.js";
 const playProgress = document.querySelector(".play-progress"),
   volumeProgress = document.querySelector(".volume-progress");
 playProgress.addEventListener("input", progressAnimation);
 volumeProgress.addEventListener("input", progressAnimation);
 
-import { createRipple } from "./tickets.js";
+import { createRipple } from "./bookingTickets.js";
 const buttons = document.getElementsByClassName("modal-btn");
 for (const button of buttons) {
   button.addEventListener("click", createRipple);
 }
 
-import initComparisons from "./explore.js";
-initComparisons();
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia3h6d3MiLCJhIjoiY2t1Y3F3NXNsMTBhejJ3bXh3bDkyOGRlZyJ9.G3CiV9AC0NrhMVhdMDgqBQ";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/light-v10", // style URL
+  center: [2.3364, 48.86091], // starting position [lng, lat]
+  zoom: 15.8, // starting zoom
+});
+map.addControl(new mapboxgl.NavigationControl());
 
+const marker1 = new mapboxgl.Marker({ color: "black" })
+  .setLngLat([2.3364, 48.86091])
+  .addTo(map);
+
+const marker2 = new mapboxgl.Marker({ color: "grey" })
+  .setLngLat([2.3333, 48.8602])
+  .addTo(map);
+
+const marker3 = new mapboxgl.Marker({ color: "grey" })
+  .setLngLat([2.3397, 48.8607])
+  .addTo(map);
+
+const marker4 = new mapboxgl.Marker({ color: "grey" })
+  .setLngLat([2.333, 48.8619])
+  .addTo(map);
+
+const marker5 = new mapboxgl.Marker({ color: "grey" })
+  .setLngLat([2.3365, 48.8625])
+  .addTo(map);
--- museum-dom/js/tickets.js
+++ museum-dom/js/tickets.js
@@ -1,23 +1,9 @@
-//ripple-effect in tickets
-function createRipple(event) {
-  const button = event.currentTarget;
+let radios = document.querySelectorAll('input[name="ttype"]');
 
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
-
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-  circle.classList.add("ripple");
-
-  const ripple = button.getElementsByClassName("ripple")[0];
-
-  if (ripple) {
-    ripple.remove();
+radios.addEventListener("click", function () {
+  for (let radio of radios) {
+    if (radio.checked) {
+      console.log(radio.value);
+    }
   }
-
-  button.appendChild(circle);
-}
-
-export { createRipple };
+});

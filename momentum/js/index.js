ea6549d6c339ee285e828eadd55e702abf773ce2
Alena Bulanova <kxzws54@gmail.com>
2021-10-25 18:58:18 +0300

    feat: add cool audio player (without opportunity to play audio by click from list under the player)

--- momentum/js/index.js
+++ momentum/js/index.js
@@ -3,26 +3,32 @@ import { getTimeOfDay } from "./greeting.js";
 import { getSlideNext, getSlidePrev, setBg, randomNum } from "./slider.js";
 import { getWeather, city } from "./weather.js";
 import { getQuotes } from "./quote.js";
-import { playAudio, playNext, playPrev, playBtn, createPlayList, audio } from "./player.js";
-
+import {
+  playAudio,
+  playNext,
+  playPrev,
+  playBtn,
+  createPlayList,
+  audio
+} from "./player.js";
 
 const dateObj = new Date();
 const hours = dateObj.getHours();
-const slideNext = document.querySelector('.slide-next');
-const slidePrev = document.querySelector('.slide-prev');
-const changeQuote = document.querySelector('.change-quote');
-const playNextBtn = document.querySelector('.play-next');
-const playPrevBtn = document.querySelector('.play-prev');
+const slideNext = document.querySelector(".slide-next");
+const slidePrev = document.querySelector(".slide-prev");
+const changeQuote = document.querySelector(".change-quote");
+const playNextBtn = document.querySelector(".play-next");
+const playPrevBtn = document.querySelector(".play-prev");
 
 showTime();
 setBg(getTimeOfDay(hours), String(randomNum).padStart(2, "0"));
 createPlayList();
 
-slideNext.addEventListener('click', getSlideNext);
-slidePrev.addEventListener('click', getSlidePrev);
-city.addEventListener('change', getWeather);
-changeQuote.addEventListener('click', getQuotes);
-playBtn.addEventListener('click', playAudio);
-playNextBtn.addEventListener('click', playNext);
-playPrevBtn.addEventListener('click', playPrev);
-audio.addEventListener('ended', playNext);
+slideNext.addEventListener("click", getSlideNext);
+slidePrev.addEventListener("click", getSlidePrev);
+city.addEventListener("change", getWeather);
+changeQuote.addEventListener("click", getQuotes);
+playBtn.addEventListener("click", playAudio);
+playNextBtn.addEventListener("click", playNext);
+playPrevBtn.addEventListener("click", playPrev);
+audio.addEventListener("ended", playNext);
--- momentum/js/player.js
+++ momentum/js/player.js
@@ -2,10 +2,16 @@ import playList from "./playList.js";
 
 const playBtn = document.querySelector(".play");
 const playUl = document.querySelector(".play-list");
+const progressBar = document.querySelector(".progress-bar");
 const audio = new Audio();
 let isPlay = false,
   playNum = 0,
   playListCount = playList.length;
+const currentSong = document.querySelector(".current-song");
+const currentTime = document.querySelector(".current-time");
+const durationTime = document.querySelector(".duration-time");
+const volBtn = document.querySelector(".volume");
+const progressVol = document.querySelector(".progress-vol");
 
 function createPlayList() {
   playList.forEach((el) => {
@@ -16,6 +22,7 @@ function createPlayList() {
   });
   const audios = document.querySelectorAll(".play-item");
   audios[0].classList.add("item-active");
+  currentSong.textContent = audios[0].textContent;
 }
 
 function toggleBtn() {
@@ -37,6 +44,7 @@ function playAudio() {
     isPlay = true;
     toggleBtn();
   }
+  setInterval(updateProgressValue, 500);
 }
 
 function playNext() {
@@ -47,6 +55,7 @@ function playNext() {
   else playNum = 0;
 
   audios[playNum].classList.add("item-active");
+  currentSong.textContent = audios[playNum].textContent;
 
   isPlay = false;
   playAudio();
@@ -60,9 +69,59 @@ function playPrev() {
   else playNum = playListCount - 1;
 
   audios[playNum].classList.add("item-active");
+  currentSong.textContent = audios[playNum].textContent;
 
   isPlay = false;
   playAudio();
 }
+
+function formatTime(seconds) {
+  let min = Math.floor(seconds / 60);
+  let sec = Math.floor(seconds - min * 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min}:${sec}`;
+}
+
+function updateProgressValue() {
+  progressBar.max = audio.duration;
+  progressBar.value = audio.currentTime;
+  const value = (audio.currentTime / audio.duration) * 100;
+  progressBar.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 ${value}%,
+     #fff ${value}%, white 100%)`;
+  currentTime.textContent = formatTime(Math.floor(audio.currentTime));
+  if (durationTime.textContent === "NaN:NaN") {
+    durationTime.textContent = "0:00";
+  } else {
+    durationTime.textContent = formatTime(Math.floor(audio.duration));
+  }
+}
+
+progressBar.addEventListener("input", () => {
+  audio.currentTime = progressBar.value;
+});
+volBtn.addEventListener("click", () => {
+  if (audio.muted) {
+    audio.muted = false;
+    volBtn.classList.remove("volume-off");
+    progressVol.value = audio.volume * 100;
+    progressVol.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 ${progressVol.value}%,
+      #fff ${progressVol.value}%, white 100%)`;
+  } else {
+    audio.muted = true;
+    volBtn.classList.add("volume-off");
+    progressVol.value = 0;
+    progressVol.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 0%,
+      #fff 0%, white 100%)`;
+  }
+});
+progressVol.addEventListener("input", () => {
+  audio.volume = progressVol.value / 100;
+  progressVol.style.background = `linear-gradient(to right, #c5b358 0%, #c5b358 ${progressVol.value}%,
+    #fff ${progressVol.value}%, white 100%)`;
+  if (audio.volume === 0) volBtn.classList.add("volume-off");
+  else volBtn.classList.remove("volume-off");
+});
 
 export { playAudio, playNext, playPrev, playBtn, createPlayList, audio };

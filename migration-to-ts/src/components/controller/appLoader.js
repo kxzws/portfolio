55a8e43831c9730f844d8a0cf233ee98060cc084
Alena Bulanova <kxzws54@gmail.com>
2021-12-06 21:52:30 +0300

    init: start migration-to-ts task

---
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: '409271144cfd438b96b70ca79bd289c1', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;

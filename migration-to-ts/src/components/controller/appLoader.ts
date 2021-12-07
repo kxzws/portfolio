c4f1aaf88dc4e47d2145c0f17756680b4a542561
Alena Bulanova <kxzws54@gmail.com>
2021-12-07 13:54:55 +0300

    fix: change url in appLoader for deploy

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', { // https://newsapi.org/v2/
             apiKey: '409271144cfd438b96b70ca79bd289c1', // получите свой ключ https://newsapi.org/
         });
     }

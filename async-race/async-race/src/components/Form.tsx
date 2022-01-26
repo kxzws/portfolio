4f83fd84ccd2b71745982a1958425b01802ccb68
Alena Bulanova <kxzws54@gmail.com>
2022-01-26 15:11:15 +0300

    feat: add form appearance

--- async-race/async-race/src/components/Form.tsx
+++ async-race/async-race/src/components/Form.tsx
@@ -1,9 +1,23 @@
-import './Form.scss';
+import "./Form.scss";
 
 function Form() {
   return (
     <div className="form">
-      
+      <div className="form__row">
+        <input type="text" className="form__input" placeholder="Car model" />
+        <input type="color" className="form__input input_color" />
+        <button className="form__btn">Create</button>
+      </div>
+      <div className="form__row">
+        <input type="text" className="form__input" placeholder="Car model" />
+        <input type="color" className="form__input input_color" />
+        <button className="form__btn">Update</button>
+      </div>
+      <div className="form__row">
+        <button className="form__btn btn_race">Race</button>
+        <button className="form__btn btn_race">Reset</button>
+        <button className="form__btn">Generate cars</button>
+      </div>
     </div>
   );
 }
